26a16d83508ed641fc13a0b3619f9fdf2ce8e404
nickingnova <nickingnova@LAPTOP-NICO.localdomain>
2021-09-08 11:09:52 -0500

    Last Deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ function Footer (){
 		return (
 			<React.Fragment>
 				<footer className="footer-container">
-					<h2 className="footer-tittle">By<a className="logo-container" target="_blank" href="/"><img className="logo-nick" src={logoNick} alt="logo-nick"/></a></h2>
+					<h2 className="footer-tittle">By<a className="logo-container" target="_blank" rel="noreferrer" href="https://nickingnova.github.io/"><img className="logo-nick" src={logoNick} alt="logo-nick"/></a></h2>
 					<ul className="social-container">
-						<li className="icons-container"><a className="icons"  target="_blank" href="/">Twitter</a></li>
-						<li className="icons-container"><a className="icons" target="_blank" href="/">Linkedin</a></li>
-						<li className="icons-container"><a className="icons" target="_blank" href="/">GitHub</a></li>
+						<li className="icons-container"><a className="icons" target="_blank" rel="noreferrer" href="https://twitter.com/nickingnova">Twitter</a></li>
+						<li className="icons-container"><a className="icons" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/nicolas-fernandez-6b4b23b1/">Linkedin</a></li>
+						<li className="icons-container"><a className="icons" target="_blank" rel="noreferrer" href="https://github.com/nickingnova">GitHub</a></li>
 					</ul>
 					<a className="promo-container" target="blank" href="https://rollercoin.com/?r=kc041d0j">
 						<img className="promo" src={promotion} alt="RollerPromo"/>
